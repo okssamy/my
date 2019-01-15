b5807deb97f3cb0d7e8424d5c3ca1cbb90aaaee3
k306002@CORP.KOREACB.KR <k306002@CORP.KOREACB.KR>
2019-01-15 14:19:57 +0900

    second

--- src/main/java/com/example/my/microservice/CourseRepository.java
+++ src/main/java/com/example/my/microservice/CourseRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * Created by uengine on 2018. 12. 19..
  */
 public interface CourseRepository extends PagingAndSortingRepository<Course, Long> {
+
+    system.out.ptintln("test");
+
+
 }
